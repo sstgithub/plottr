92bc990001781cbeaeabb3cfa7b1304048390270
Sid Tantia <sstantia@gmail.com>
2017-02-24 20:38:18 -0800

    Better names; green tests

--- lib/plottr.rb
+++ lib/plottr.rb
@@ -3,17 +3,17 @@ require "plottr/version"
 module Plottr
   class Scatter
     def initialize(coords_array)
-      gen_blank_scatter_plot(coords_array)
-      add_coords_to_blank_plot(coords_array)
+      gen_blank_graph(coords_array)
+      add_coords_to_blank_graph(coords_array)
     end
 
-    def gen_scatter_plot_as_string
+    def gen_string_scatter_plot
       " ^\n" + @current_scatter_plot.reverse.join("\n") + "\n" +  @bottom_of_scatter_plot.join("\n")
     end
 
     private
 
-    def add_coords_to_blank_plot(coords_array)
+    def add_coords_to_blank_graph(coords_array)
       coords_array.each do |coords|
         row = @current_scatter_plot[coords.last]
         cols = row.split("")
@@ -26,10 +26,10 @@ module Plottr
       end
     end
 
-    def gen_blank_scatter_plot(coords_array)
+    def gen_blank_graph(coords_array)
       highest_x_val = find_highest_x(coords_array)
       highest_y_val = find_highest_y(coords_array)
-      @bottom_of_scatter_plot = gen_bottom_of_scatter_plot(highest_x_val)
+      @bottom_of_scatter_plot = gen_bottom_of_graph(highest_x_val)
       @current_scatter_plot = []
       (highest_y_val+1).times do |i|
         @current_scatter_plot << "#{i}|"+(" " * (highest_x_val*2))
@@ -44,7 +44,7 @@ module Plottr
       coords_array.map(&:first).max
     end
 
-    def gen_bottom_of_scatter_plot(highest_x_val)
+    def gen_bottom_of_graph(highest_x_val)
       #each num on bottom for x vals should have be separated by a space
       numbering_for_x_vals = "  "
       (highest_x_val+1).times do |i|
--- test/plottr_test.rb
+++ test/plottr_test.rb
@@ -8,11 +8,13 @@ class PlottrTest < Minitest::Test
 
   def test_that_it_can_draw_scatter_plot_for_one_coord
     scatter_plot = Plottr::Scatter.new(@one_coord)
-    assert_equal " ", scatter_plot.gen_scatter_plot_as_string
+    expected_string_scatter_plot = " ^\n1|  x\n0|  \n  -->\n  0 1 "
+    assert_equal expected_string_scatter_plot, scatter_plot.gen_string_scatter_plot
   end
 
   def test_that_it_can_draw_scatter_plot_for_mult_coords
     scatter_plot = Plottr::Scatter.new(@mult_coords)
-    assert_equal " ", scatter_plot.gen_scatter_plot_as_string
+    expected_string_scatter_plot = " ^\n6|          x\n5|          \n4|          \n3|  x       \n2|          \n1|  x       x\n0|          \n  ---------->\n  0 1 2 3 4 5 "
+    assert_equal expected_string_scatter_plot, scatter_plot.gen_string_scatter_plot
   end
 end
